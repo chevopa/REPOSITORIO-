e784294298738ffa6371493d2ff74ebce5c7e2fe
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2024-08-09 20:47:42 -0500

    ##

---
+++ MOMENTO 1/PUNTO 2.py
@@ -0,0 +1,7 @@
+print("Litros de leche producidos en un dia")
+litros_leche = input(" digite la cantidad de leche: ")
+
+print("Conversion de litros a galones")
+conversion = int(litros_leche)*float(3.785)
+
+print(f' la cantidad de leche en ganoles es de {conversion}')
